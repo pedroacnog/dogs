56a6296d4e287d3528460d483aeea2c08cbbf3f5
pedro <pedroacnog@gmail.com>
2022-01-19 12:23:21 -0300

    add header

--- src/Components/Footer/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import styles from './styles.css'
-
-export default function Footer() {
-  return <div>Footer</div>;
-}
--- src/Components/Login/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import styles from './styles.css'
-
-export default function Login() {
-  return <div></div>;
-}
